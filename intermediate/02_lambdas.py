22a2b860b61ca6e9c3ee6a141155f87ebcd1886e
cecilia <ravacecilia1@gmail.com>
2025-07-08 18:54:22 -0300

    feat: Learn about lambdas and higher order functions

---
+++ intermediate/02_lambdas.py
@@ -0,0 +1,11 @@
+sum_two_values = lambda number_one, number_two: number_one + number_two
+print(sum_two_values(2, 4))
+
+multiply_values = lambda number_one, number_two: number_one * number_two - 3
+print(multiply_values(2, 4))
+
+def sum_three_values(value):
+    return lambda number_one, number_two: number_one + number_two + value
+
+
+print(sum_three_values(5)(2, 4))
---
+++ intermediate/03_higher_order_functions.py
@@ -0,0 +1,10 @@
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+
+def sum_two_values_and_add_value(number_one, number_two, f):
+    return f(number_one + number_two)
+print(sum_two_values_and_add_value(5, 2, sum_one))
+print(sum_two_values_and_add_value(5, 2, sum_five))
